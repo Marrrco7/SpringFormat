bb4eaa39718d8dfd4f504dfb80e708d189d5afbc
Marco <marcodomenico.zv@gmail.com>
2025-05-05 12:42:49 +0200

    first commit

---
+++ src/main/java/com/example/s30019tpo08/S30019Tpo08Application.java
@@ -0,0 +1,13 @@
+package com.example.s30019tpo08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30019Tpo08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S30019Tpo08Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/s30019tpo08/S30019Tpo08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s30019tpo08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30019Tpo08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
